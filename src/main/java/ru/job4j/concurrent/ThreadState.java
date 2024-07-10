d32f1b36508e98e52e302d578552b9c3a7fb5045
stkuper <stklipper@gmail.com>
2024-07-11 00:21:43 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,21 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName()
+                )
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+               && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName());
+            System.out.println(second.getName());
+        }
+        System.out.printf("%s %s", Thread.currentThread().getName(), "Работа завершена");
+    }
+}

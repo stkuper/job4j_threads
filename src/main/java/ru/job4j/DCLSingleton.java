6a39abb1ab094c3775ad46ec54f2d38871033663
stkuper <stklipper@gmail.com>
2024-07-29 22:45:20 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/ru/job4j/DCLSingleton.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton instance;
+
+    public static DCLSingleton getInstance() {
+        if (instance == null) {
+            synchronized (DCLSingleton.class) {
+                if (instance == null) {
+                    instance = new DCLSingleton();
+                }
+            }
+        }
+        return instance;
+    }
+
+    private DCLSingleton() {
+    }
+}

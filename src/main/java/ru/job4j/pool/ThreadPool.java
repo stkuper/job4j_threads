eccaac1071141c07ca1aeca9b2764688d1e3548d
stkuper <stklipper@gmail.com>
2024-09-03 22:32:47 +0300

    1. Реализовать ThreadPool [#1099]

---
+++ src/main/java/ru/job4j/pool/ThreadPool.java
@@ -0,0 +1,52 @@
+package ru.job4j.pool;
+
+import ru.job4j.blockingqueue.SimpleBlockingQueue;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreadPool {
+    private final int size = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> threads = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(size / 2);
+
+    public ThreadPool() {
+        for (int i = 0; i < size; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        Runnable task = tasks.poll();
+                        if (task != null) {
+                            task.run();
+                        }
+                    }
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+            threads.add(thread);
+            thread.start();
+        }
+    }
+
+    public void work(Runnable job) {
+        try {
+            tasks.offer(job);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
